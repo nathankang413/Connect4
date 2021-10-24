6fb7331d089a0dacd1d6ad5e423fda32a16c6631
nathangong <nathangong1234@gmail.com>
2021-10-23 19:10:24 -0700

    Update display to functionally display a board

--- src/game/Display.java
+++ src/game/Display.java
@@ -1,61 +1,70 @@
 package game;
 
-import java.awt.Color;
-import acm.program.GraphicsProgram;
-import acm.graphics.GRect;
 import acm.graphics.GOval;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-import static game.Constants.Game.*;
+import java.awt.*;
+
 import static game.Constants.GUI.*;
+import static game.Constants.Game.*;
 
 public class Display extends GraphicsProgram {
-    
+    private int[][] board; // TODO: use something else to get the actual board state in the game
+
     public static void main(String[] args) {
         new Display().start(args);
-
         System.out.println("here");
-
 
     }
 
-    public void run() {
+    private Display() {
+        board = new int[ROWS][COLS];
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                board[i][j] = i % 2;
+            }
+        }
+    }
 
+    public void run() {
         Column[] frame = new Column[COLS];
-        for (int i=0; i<COLS; i++) {
-            frame[i] = new Column(i*SPACING, 0);
+        for (int i = 0; i < COLS; i++) {
+            frame[i] = new Column(i * SPACING, 0);
             add(frame[i]);
         }
 
         Position[][] positions = new Position[ROWS][COLS];
-        for (int i=0; i<ROWS; i++) {
-            for (int j=0; j<COLS; j++) {
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
 
-                positions[i][j] = new Position(j*SPACING + MARGIN, i*SPACING + MARGIN);
+                positions[i][j] = new Position(j * SPACING + MARGIN, i * SPACING + MARGIN, board[i][j]);
                 add(positions[i][j]);
 
             }
         }
     }
-}
 
-class Position extends GOval {
+    private class Position extends GOval {
+        public Position(int x, int y, int type) {
+            super(x, y, DIAMETER, DIAMETER);
 
-    public Position(int x, int y) {
-        super(x, y, DIAMETER, DIAMETER);
-
-        setFillColor(Color.LIGHT_GRAY);
-        setFilled(true);
+            if (type == EMPTY)
+                setFillColor(Color.LIGHT_GRAY);
+            else if (type == PLAYER_1)
+                setFillColor(Color.YELLOW);
+            else
+                setFillColor(Color.RED);
+            setFilled(true);
+        }
     }
 
-}
+    private class Column extends GRect {
+        public Column(int x, int y) {
+            super(x, y, SPACING, ROWS * (SPACING));
 
-class Column extends GRect {
-
-    public Column (int x, int y) {
-        super(x, y, SPACING, ROWS*(SPACING));
-
-        setFillColor(Color.BLUE);
-        setFilled(true);
+            setFillColor(Color.BLUE);
+            setFilled(true);
+        }
     }
-
 }
