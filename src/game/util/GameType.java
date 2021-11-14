c6fec57eb31956d7acef67b036da7579ca3c2374
Nathan Kang <nathankang413@gmail.com>
2021-11-14 08:46:37 -0800

    improved QLearn logic, added new training options (only new moves)

--- src/game/util/GameType.java
+++ src/game/util/GameType.java
@@ -7,16 +7,17 @@ public class GameType {
     public final static int HUMAN = 0;
     public final static int ALGORITHM = 1;
     public final static int Q_LEARN = 2;
-    public final static int Q_LEARN_RAND = 3;
+    public final static int Q_LEARN_NEW = 3;
+    public final static int Q_LEARN_RAND = 4;
     private final int[] playerTypes;
 
     public GameType(int player1, int player2) {
-        // 0 - human, 1 - Algo, 2 - QLearn, 3 - QLearn-onlyRand
 
-        if (player1 > 3 || player1 < 0) {
+        // TODO: remove magic numbers
+        if (player1 > 4 || player1 < 0) {
             throw new IllegalArgumentException("player1 type " + player1 + " is invalid.");
         }
-        if (player2 > 3 || player2 < 0) {
+        if (player2 > 4 || player2 < 0) {
             throw new IllegalArgumentException("player2 type " + player2 + " is invalid.");
         }
 
@@ -31,10 +32,11 @@ public class GameType {
 
         for (int i=0; i<2; i++) {
             switch (playerTypes[i]) {
-                case 0 -> players[(i + startPlayer) % 2] = new HumanPlayer();
-                case 1 -> players[(i + startPlayer) % 2] = new AlgorithmicPlayer();
-                case 2 -> players[(i + startPlayer) % 2] = new QLearnPlayer();
-                case 3 -> players[(i + startPlayer) % 2] = new QLearnPlayer(true);
+                case HUMAN -> players[(i + startPlayer) % 2] = new HumanPlayer();
+                case ALGORITHM -> players[(i + startPlayer) % 2] = new AlgorithmicPlayer();
+                case Q_LEARN -> players[(i + startPlayer) % 2] = new QLearnPlayer(QLearnPlayer.NORMAL);
+                case Q_LEARN_NEW -> players[(i + startPlayer) % 2] = new QLearnPlayer(QLearnPlayer.NEW);
+                case Q_LEARN_RAND -> players[(i + startPlayer) % 2] = new QLearnPlayer(QLearnPlayer.RANDOM);
             }
         }
 
